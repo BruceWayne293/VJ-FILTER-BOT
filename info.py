cc15d72404588971489cb8d43cec27f887ceaf37
BruceWayne293 <foryournothing@gmail.com>
2025-05-21 23:19:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,8 +113,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', False))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', '')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'LinkCents.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'bdd6d346a92b774af1bd8a6727a9cbbde725c801')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
