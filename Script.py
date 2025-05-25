771bb347df7759e8f793521f57a31b2ab0ad0511
BruceWayne293 <foryournothing@gmail.com>
2025-05-25 13:43:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,7 +244,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b><a href="https://t.me/KL55_LINKS">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/KL55_LINKS>[KL55_LINKS]</a>
 
 <b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
